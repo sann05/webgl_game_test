f623626caff6ab1bffc41cab48cc8a964cdcaeb4
AndrejKorubov <andrejkorubov@gmail.com>
2025-03-12 21:00:40 +0000

    test

--- unity-loader.js
+++ unity-loader.js
@@ -27,15 +27,15 @@ function unityShowBanner(msg, type) {
 
 // Unity build configuration
 var buildUrl = "Build";
-var loaderUrl = buildUrl + "/WebGL_1.4.loader.js";
+var loaderUrl = buildUrl + "/WebGL_TEST_Tutorial.loader.js";
 var config = {
-    dataUrl: buildUrl + "/4bdef2527e1267f7f0e14692d00f840d.data.unityweb",
+    dataUrl: buildUrl + "/1a9df536a2236589843a100ba9be647f.data.unityweb",
     frameworkUrl: buildUrl + "/2bcd764036bc9683b834babd60fd1f87.js.unityweb",
     codeUrl: buildUrl + "/97f1730fa47acdbd55e1f9ae51219e91.wasm.unityweb",
     streamingAssetsUrl: "StreamingAssets",
     companyName: "KorubovGames",
     productName: "ZombieTrain_Prod",
-    productVersion: "1.404",
+    productVersion: "1.405",
     showBanner: unityShowBanner,
 };
 
