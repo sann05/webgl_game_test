5475f77d013eee11967d7237bc7e5299839c575e
Nikita Ischenko <107185196+DragonAirDragon@users.noreply.github.com>
2025-03-26 14:05:33 +0300

    Test VContainer

--- unity-loader.js
+++ unity-loader.js
@@ -29,13 +29,13 @@ function unityShowBanner(msg, type) {
 var buildUrl = "Build";
 var loaderUrl = buildUrl + "/9853637125e801e9aae48e78dbbdcfca.loader.js";
 var config = {
-    dataUrl: buildUrl + "/62ddc8829b44626d9a8f9c4050a68a55.data.unityweb",
-    frameworkUrl: buildUrl + "/eebb08c80037ef4812d972ec6afb0ddb.framework.js.unityweb",
-    codeUrl: buildUrl + "/12eb38296effda2c99bbf22905f08049.wasm.unityweb",
+    dataUrl: buildUrl + "/377cee2322d0cf1db94fc13e591cc777.data.unityweb",
+    frameworkUrl: buildUrl + "/a9dc8802c1ef7ab372defbdcae231e51.framework.js.unityweb",
+    codeUrl: buildUrl + "/e667196bb6781736f64bc40bb7f1405e.wasm.unityweb",
     streamingAssetsUrl: "StreamingAssets",
     companyName: "KorubovGames",
     productName: "ZombieTrain_Prod",
-    productVersion: "1.459",
+    productVersion: "1.460",
     showBanner: unityShowBanner,
 };
 
