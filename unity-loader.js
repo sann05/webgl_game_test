0c344ad5550c6edaa259591a4eab952967e3bb85
Nikita Ischenko <107185196+DragonAirDragon@users.noreply.github.com>
2025-03-27 16:54:23 +0300

    Update unity-loader.js

--- unity-loader.js
+++ unity-loader.js
@@ -34,17 +34,13 @@ var loaderUrl = buildUrl + "/{{{ LOADER_FILENAME }}}";
 var config = {
     dataUrl: buildUrl + "/{{{ DATA_FILENAME }}}",
     frameworkUrl: buildUrl + "/{{{ FRAMEWORK_FILENAME }}}",
-    #if USE_THREADS
     workerUrl: buildUrl + "/{{{ WORKER_FILENAME }}}",
-    #endif
-#if USE_WASM
     codeUrl: buildUrl + "/{{{ CODE_FILENAME }}}",
-    #endif
     streamingAssetsUrl: "StreamingAssets",
-    companyName: {{{ JSON.stringify(COMPANY_NAME) }}},
-productName: { { { JSON.stringify(PRODUCT_NAME) } } },
-productVersion: { { { JSON.stringify(PRODUCT_VERSION) } } },
-showBanner: unityShowBanner,
+    companyName: "{{{ COMPANY_NAME }}}",
+    productName: "{{{ PRODUCT_NAME }}}",
+    productVersion: "{{{ PRODUCT_VERSION }}}",
+    showBanner: unityShowBanner,
 };
 
 // Mobile device configuration
