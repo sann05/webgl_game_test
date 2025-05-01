3362f8dc933020eeea1f05c78c5856d9f49b76e2
AndrejKorubov <andrejkorubov@gmail.com>
2025-05-01 05:07:17 +0300

    Update Road Loot

--- unity-loader.js
+++ unity-loader.js
@@ -29,13 +29,13 @@ function unityShowBanner(msg, type) {
 var buildUrl = "Build";
 var loaderUrl = buildUrl + "/9853637125e801e9aae48e78dbbdcfca.loader.js";
 var config = {
-    dataUrl: buildUrl + "/e0f606ae360f75825daa94126b7d6554.data.unityweb",
-    frameworkUrl: buildUrl + "/29881cd8572c2de8f5f05c933550538e.framework.js.unityweb",
-    codeUrl: buildUrl + "/7b8ff2151f614850255e3e9c0fcf8efd.wasm.unityweb",
+    dataUrl: buildUrl + "/bc63d1ae4bb5e38052387e93b5cf367d.data.unityweb",
+    frameworkUrl: buildUrl + "/e26e7ddb1e19c80bfc6784b4b55cbe00.framework.js.unityweb",
+    codeUrl: buildUrl + "/f95975c51893a8f064fbdb593cf41dc4.wasm.unityweb",
     streamingAssetsUrl: "StreamingAssets",
     companyName: "KorubovGames",
     productName: "ZombieTrain_Prod",
-    productVersion: "1.539",
+    productVersion: "1.569",
     showBanner: unityShowBanner,
 };
 
